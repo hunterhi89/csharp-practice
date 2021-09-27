2b006abce55defc4860db4d0857416501408c396
Hannah Hunter <78035073+v-hhunter@users.noreply.github.com>
2021-09-27 13:13:51 -0400

    friday training

---
+++ HelloWorld/HelloWorld/Dog.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    
+    public class Animal
+    {
+        public bool HasCells { get; private set; } = true;
+    }
+    
+    public class Mammal : Animal
+    {
+        private int _numberOfEars = 2;
+        public int NumberOfEars
+        {
+            get
+            {
+                return _numberOfEars;
+            }
+            set
+            {
+                if (value >= 0 && value <= 2)
+                {
+                    _numberOfEars = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number of ears.");
+                }
+
+
+            }
+        }
+
+    }
+    public class Dog : Mammal
+    {
+        public Dog(string name = "hambone")
+        {
+            Name = name;
+            Console.WriteLine("I am creating a dog for you named " + name);
+        }
+
+
+
+        public int NumberOfEyes { get; } = 2;
+
+
+
+        public int NumberOfLegs = 4;
+        public void LegCheck() 
+        {
+            Console.WriteLine("This dog has " + NumberOfLegs + " legs.");
+        }
+        public string Name = null;
+        public void NameCheck() 
+        {
+            if (Name != null)
+            {
+                Console.WriteLine("The dog's name is " + Name);
+            }
+            else
+            {
+                Console.WriteLine("Please name this dog");
+            }
+            
+            
+        }
+
+    }
+}
---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HelloWorld
+{
+
+    public class Person
+    {
+        public int Age;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i <= 100; i++)
+                if (i/3 == 0)
+            {
+                    Console.WriteLine(i);
+            }
+
+
+
+            Dog myDog = new Dog("baloo");
+            myDog.NumberOfEars = 1;
+            int x = myDog.NumberOfEars;
+            Cat myCat = new Cat();
+            
+            // myDog.NumberOfEyes = 3;
+
+            Dog myOtherDog = new Dog();
+
+
+        }
+
+    }
+}
