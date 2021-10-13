4988a05771eb2ad636a3560bb1b1638637f70079
Hannah Hunter <78035073+v-hhunter@users.noreply.github.com>
2021-10-13 15:34:42 -0500

    exercises for date, time, and strings

---
+++ Dates Exercises/Dates Exercises/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Dates_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var dateTime = new DateTime(2021, 1, 1);
+            //var now = DateTime.Now;
+            //var today = DateTime.Today;
+
+            //Console.WriteLine("Hour: " + now.Hour);
+            //Console.WriteLine("Minute: " + now.Minute);
+
+            //var tomorrow = now.AddDays(1);
+            //var yesterday = now.AddDays(-1);
+
+            //Console.WriteLine(now.ToLongDateString());
+            //Console.WriteLine(now.ToShortDateString());
+            //Console.WriteLine(now.ToLongTimeString());
+            //Console.WriteLine(now.ToShortTimeString());
+
+            //Console.WriteLine(now.ToString("yy-MM-dd"));
+
+            var timeSpan = new TimeSpan(1, 2, 3);
+
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration: " + duration);
+
+            Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            Console.WriteLine("Total minutes: " + timeSpan.TotalMinutes);
+
+            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+            Console.WriteLine("Subtract Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(2)));
+
+            Console.WriteLine("ToString: " + timeSpan.ToString());
+
+            Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03"));
+        }
+    }
+}
---
+++ Text Exercises/Text Exercises/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Text_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Hannah Hunter ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("First Name: " + firstName);
+            Console.WriteLine("Last Name: " + lastName);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("First Name: " + names[0]);
+            Console.WriteLine("Last Name: " + names[1]);
+
+            Console.WriteLine(fullName.Replace("Hannah", "Hannah Isabel"));
+
+            if (String.IsNullOrWhiteSpace(" "))
+                Console.WriteLine("Invalid");
+
+            var str = "25";
+            var age = Convert.ToByte(str);
+            Console.WriteLine(age);
+
+            float price = 29.95f;
+            price.ToString("C0");
+            Console.WriteLine(price.ToString("C0"));
+
+            //Summarizing Text
+            var sentence = "Congrats, you have started reading a super duper, really obnoxious, awfully long run-on sentence with absoluetly no end in sight.";
+            var summary = StringUtility.SummarizeText(sentence);
+            Console.WriteLine(summary);
+
+        }
+
+    }
+}
